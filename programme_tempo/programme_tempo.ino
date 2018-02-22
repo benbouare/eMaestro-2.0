dc110c3e6e6fb5df631c79ea350343a7da59b255
Adama Ben Bouaré <bouareben@outlook.fr>
2018-02-22 14:50:43 +0100

    programme arduino après poto diapason

--- programme_tempo/programme_tempo.ino
+++ programme_tempo/programme_tempo.ino
@@ -1,5 +1,6 @@
 #include <MIDI.h>
 
+//gestion MIDI USB
 #if defined(USBCON)
 #include <midi_UsbTransport.h>
 
@@ -10,7 +11,7 @@ UsbTransport sUsbTransport;
 
 MIDI_CREATE_INSTANCE(UsbTransport, sUsbTransport, MIDI);
 
-#else // No USB available, fallback to Serial
+#else // Pas de connexion USB, retour à la connexion Série
 MIDI_CREATE_DEFAULT_INSTANCE();
 #endif
 
@@ -41,37 +42,48 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 #include <pixeltypes.h>
 #include <platforms.h>
 #include <power_mgt.h>
-
 #include <Boards.h>
 #include <Firmata.h>
 #include <FirmataConstants.h>
 #include <FirmataDefines.h>
 #include <FirmataMarshaller.h>
 #include <FirmataParser.h>
-
 #include "FastLED.h"
-#define NUM_LEDS 49
-// Data pin that led data will be written out over
-#define DATA_PIN 3
 
-CRGB leds[NUM_LEDS];
-// --
-int tabRien[25];
-int tabBinaire[13];
-int tabTernaire[9];
-int decalageRien[25];
-int decalageBinaire[13];
-int decalageTernaire[9];
+#define NUM_LEDS 49 // Nombre de leds de la guirlande
+#define DATA_PIN 3 // Data pin de la guirlande de leds
+#define MAX_BRIGHTNESS 255 // Luminosité minimale
+#define MIN_BRIGHTNESS 16 // Luminosité maximale
+
+CRGB leds[NUM_LEDS]; // Tableau dont chaque élément référence chaque led de la guirlande.
+
+// tableaux de nombres de leds à allumer en fonction du nombre de temps par mesure
+int tabRien[25]; // pour le mode unaire
+int tabBinaire[13]; // pour le mode binaire
+int tabTernaire[9]; // pour le mode ternaire
+
+//tableaux définissant le nombre de leds occupées pour chaque temps de la mesure
+int decalageRien[25]; // pour le mode unaire
+int decalageBinaire[13]; // pour le mode binaire
+int decalageTernaire[9]; // pour le mode ternaire
 
 
-int nbLed; // nombre de leds à allumer
-int mode; // 1-rien, 2-binaire, 3-ternaire
-int decalage; // espacement entre les led allumées pour mieux répartir les leds alumées sur les 24
+int nbLed; // nombre de leds effectif à allumer
+int mode; // 1-rien, 2-binaire, 3-ternaire.
+int decalage; // espacement entre les leds allumées pour mieux répartir les leds alumées sur les 24.
+int  r, g, b; // codes couleurs : red, green, blue.
+int alerteR, alerteG, alerteB; // codes couleurs pour les alertes
+int brightness; // variable pour la luminosité
+int armature; // nombre de dièses - nombre de bémols
 
+/**
+ * Fonction appelée en cas de réception d'un message NoteOn
+ * Le premier message informe du mode(unaire, binaire ou ternaire) et du nombre de temps par mesure.
+ * Les autres messages servent à allumer les leds avec la couleur et la luminosité définis par les variables bright et r,g,b.
+ * Utilisée pour l'affichage du tempo
+ */
 void handleNoteOn(byte inChannel, byte inNumber, byte inVelocity)
 {
-  //premier note on pour connaitre le mode(binaire, ternaire ou neutre) et le nombre de leds à allumer
-  //2e note on pour allumer les leds
     Serial.println("Note On");
     Serial.print(inNumber);
     Serial.print(" ");
@@ -80,6 +92,7 @@ void handleNoteOn(byte inChannel, byte inNumber, byte inVelocity)
 
     int temps = inVelocity;
     switch(temps){
+      //gestion du premier message NoteOn(définition du mode et du nombre de leds en fonction du nombre de temps)
       case 25: //mode rien
         nbLed = tabRien[inNumber];
         mode = 1;
@@ -98,22 +111,68 @@ void handleNoteOn(byte inChannel, byte inNumber, byte inVelocity)
         decalage = decalageTernaire[inNumber];
         break;
 
+      //gestion des autres messages Noteon(allumage des leds)
       default:
-        int  r = 255, g = 255, b = 255;
+          //leds du tempo (leds 0-23)
           for(int i = ((inVelocity-1)*decalage) ; i < (((inVelocity-1)*decalage)+nbLed) ; i++) {
       
               leds[i] = CRGB (r,g,b); // led i
-      
-              // Show the leds
-              FastLED.show();
            ;
           };
-        /*leds[inVelocity] = CRGB (0, 0, 255);
-        FastLED.show();*/
+          
+          //leds des alertes (leds 24-39 et 48)
+          for(int i = 24; i < 40; i++){
+            leds[i] = CRGB (alerteR,alerteG,alerteB); // crimson
+          }
+          leds[48] = CRGB (alerteR,alerteG,alerteB);
+
+          //gestion des armatures 
+          if (armature > 0) // dièse > bémol
+            {
+                for (int i = 41; i < 48; i++)
+                    {
+                        leds[i] = CRGB (0,0,0);
+                    }
+                for (int i = 41; i < 41 + armature; i++)
+                {
+                    leds[i] = CRGB (r,g,b);
+                }
+            }
+            else
+            {
+                if (armature < 0) // dièse < bémol
+                {
+                    for (int i = 41; i < 48; i++)
+                    {
+                        leds[i] = CRGB (0,0,0);
+                    }
+                    for (int i = 47; i > 47 + armature; i--)
+                    {
+                        leds[i] = CRGB (r,g,b);
+                    }
+                }
+                else
+                {
+                    for (int i = 41; i < 48; i++)
+                    {
+                        leds[i] = CRGB (0,0,0);
+                    }
+                }
+            }
+          
+          //allumage des Leds
+          FastLED.setBrightness(brightness);
+          FastLED.show();
         break; 
     }
     
 }
+
+/**
+ * Fonction appelée en cas de réception d'un message Noteoff
+ * Permet d'éteindre les leds spécifiées
+ * inNumber représente le sous-temps, inVelocity représente le temps courant de la mesure.
+ */
 void handleNoteOff(byte inChannel, byte inNumber, byte inVelocity)
 {
   //pareil que pour les messages noteOn
@@ -122,10 +181,12 @@ void handleNoteOff(byte inChannel, byte inNumber, byte inVelocity)
     Serial.print(" ");
     Serial.println(inVelocity);
     Serial.println("");
-    int  r = 0, g = 0, b = 0;
+
+    //extinction des leds
+      //r = 0; g = 0; b = 0;
       for(int i = ((inVelocity-1)*decalage+(nbLed/mode)*(inNumber-1)) ; i < (((inVelocity-1)*decalage)+(nbLed/mode)*inNumber) ; i++) {
       
-              leds[i] = CRGB (r,g,b); // led i
+              leds[i] = CRGB (0,0,0); // led i
       
               // Show the leds
               FastLED.show();
@@ -136,16 +197,157 @@ void handleNoteOff(byte inChannel, byte inNumber, byte inVelocity)
   
 }
 
+/**
+ * Fonction appelée en cas de réception de message Control Change
+ * Permet de définir des paramètres globaux utilisés par l'ensemble des autres fonctions de la MaestroBox
+ * utilisée pour la gestion des nuances(définies par 8 couleurs et luminosités) avec la variable inNumber, représentant la couleur choisie.
+ * utilisée pour la gestion des alertes avec le paramètre inChannel(6 couleurs pour représenter les alertes)
+ */
+void handleControlChange(byte inChannel, byte inNumber, byte inValue){
+
+  //gestion des alertes (leds 24-39 et 48)
+  switch(inChannel){
+    case 1:
+      alerteR = 0;
+      alerteG = 0;
+      alerteB = 0;
+      break;
+      
+    case 2:
+      alerteR = 220;
+      alerteG = 20;
+      alerteB = 60;
+      break;
+      
+    case 3:
+      alerteR = 85;
+      alerteG = 107;
+      alerteB = 47;
+      break;
+      
+    case 4:
+      alerteR = 255;
+      alerteG = 20;
+      alerteB = 147;
+      break;
+      
+    case 5:
+      alerteR = 255;
+      alerteG = 250;
+      alerteB = 205;
+      break;
+      
+    case 6:
+      alerteR = 0;
+      alerteG = 0;
+      alerteB = 128;
+      break;
+      
+    case 7:
+      alerteR = 178;
+      alerteG = 34;
+      alerteB = 34;
+      break;
+      
+    default :
+      alerteR = 0;
+      alerteG = 0;
+      alerteB = 0;
+      break;
+  }
+  
+  //gestion des nuances
+  switch(inNumber){
+    case 0:
+      r = 255;
+      g = 255;
+      b = 255;
+      break;
+    
+    case 1:
+      r = 255;
+      g = 0;
+      b = 255;
+      brightness = MIN_BRIGHTNESS;
+      break;
+      
+    case 2:
+      r = 0;
+      g = 255;
+      b = 255;
+      brightness = 32;
+      break;
+      
+    case 3:
+      r = 0;
+      g = 0;
+      b = 255;
+      brightness = 48;
+      break;
+
+    case 4:
+      r = 0;
+      g = 128;
+      b = 0;
+      brightness = 80;
+      break;
+
+    case 5:
+      r = 173;
+      g = 255;
+      b = 47;
+      brightness = 126;
+      break;
+
+    case 6:
+      r = 255;
+      g = 255;
+      b = 0;
+      brightness = 180;
+      break;
+
+    case 7:
+      r = 255;
+      g = 69;
+      b = 0;
+      brightness = 220;
+      break;
+
+    case 8:
+      r = 255;
+      g = 0;
+      b = 0;
+      brightness = MAX_BRIGHTNESS;
+      break; 
+  }
+
+  /*switch(inValue){
+    
+  }*/
+}
+
+void handleAfterTouchPoly(byte inChannel, byte inNumber, byte inValue){
+  armature = inValue - inNumber; // nombre de dièses - nombre de bémols
+}
+
+/**
+ * Fonction appelée au démarrage de l'Arduino
+ * Permet d'initialiser la guirlande de leds, de paramétrer les callbacks de gestion des messages MIDI
+ * permet d'initialiser les tableaux
+ */
 void setup() {
   Serial.begin(115200);
-  FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   //while (!Serial);
   //pinMode(13, OUTPUT);
   MIDI.setHandleNoteOn(handleNoteOn);
   MIDI.setHandleNoteOff(handleNoteOff);
+  MIDI.setHandleControlChange(handleControlChange);
+  MIDI.setHandleAfterTouchPoly(handleAfterTouchPoly);
   MIDI.begin(MIDI_CHANNEL_OMNI);
   Serial.println("Arduino ready !");
 
+  //tabRien[nombreDeTemps/mesure] = nombre de leds à allumer
   tabRien[1] = 24;
   tabRien[2] = 12;
   tabRien[3] = 8;
@@ -170,7 +372,8 @@ void setup() {
   tabRien[22] = 1;
   tabRien[23] = 1;
   tabRien[24] = 1;
-  
+
+  //tabBinaire[nombreDeTemps/mesure] = nombre de leds à allumer
   tabBinaire[1] = 24;
   tabBinaire[2] = 12;
   tabBinaire[3] = 8;
@@ -183,7 +386,8 @@ void setup() {
   tabBinaire[10] = 2;
   tabBinaire[11] = 2;
   tabBinaire[12] = 2;
-  
+
+  //tabTernaire[nombreDeTemps/mesure] = nombre de leds à allumer
   tabTernaire[1] = 24;
   tabTernaire[2] = 12;
   tabTernaire[3] = 6;
@@ -193,6 +397,7 @@ void setup() {
   tabTernaire[7] = 3;
   tabTernaire[8] = 3;
 
+  //decalageTernaire[nombreDeTemps/mesure] - nombre de leds à allumer = Nombre de leds d'espacement
   decalageTernaire[1] = 24;
   decalageTernaire[2] = 12;
   decalageTernaire[3] = 8;
@@ -202,19 +407,26 @@ void setup() {
   decalageTernaire[7] = 3;
   decalageTernaire[8] = 3;
 
+  //decalageRien[nombreDeTemps/mesure] - nombre de leds à allumer = Nombre de leds d'espacement
   for(int j=1; j<25; j++){
     decalageRien[j] = tabRien[j];
   }
 
+  //decalageBinaire[nombreDeTemps/mesure] - nombre de leds à allumer = Nombre de leds d'espacement
   for(int k=1; k<13; k++){
     decalageBinaire[k] = tabBinaire[k];
     if((k == 7) || (k == 8)){
       decalageBinaire[k] = 3;
     }
   }
-
+  
 }
 
+/**
+ * Fonction appelée constamment par l'arduino durant toute l'exécution
+ * permet de recevoir des messages MIDI
+ * Permet d'appeler la fonction adéquate en fonction du type de message reçu
+ */
 void loop() {
   MIDI.read();
 }
